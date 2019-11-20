ff824d6abeca8910640b48700feb32eb30783f26
John Tucker <johntucker@Johns-MacBook-Pro.local>
2019-11-20 10:15:43 -0500

    checkpoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+    {
+        let r;
+        // println!("r: {}", r); // UN-INITIALIZED
+        {
+            let x = 5;
+            r = &x;
+        }
+    }
+    /*
+    {
+        let r;
+        {
+            let x = 5;
+            r = &x; // NOT LIVE LONG ENOUGH
+        }
+        println!("r: {}", r);
+    }
+    */
 }
