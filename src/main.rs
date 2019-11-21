9e296334ad93d1e620c038c543c5cf7825c8f28a
John Tucker <johntucker@Johns-MacBook-Pro.local>
2019-11-21 05:01:36 -0500

    checkpoint

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,29 @@
 fn main() {
     {
-        let r;
-        // println!("r: {}", r); // UN-INITIALIZED
+        let x = 5;
         {
-            let x = 5;
-            r = &x;
+            let y = &x;
         }
     }
+
+    {
+        let x = "abc";
+    }
+
+    {
+        let mut r;
+        r = 5;
+        r += 1;
+        println!("r: {}", r); // 6
+        // r = "abc"; // MISMATCHED TYPES
+    }
+
     /*
     {
         let r;
         {
             let x = 5;
-            r = &x; // NOT LIVE LONG ENOUGH
+            r = &x; // X DOES NOT LIVE LONG ENOUGH
         }
         println!("r: {}", r);
     }
